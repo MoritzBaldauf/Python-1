6bcbb6a2141671a38dc53822121fec6296be3607
Moritz <moritz.baldauf24@gmail.com>
2023-11-06 20:42:48 +0100

    First Commit

---
+++ a4_ex3.py
@@ -0,0 +1,47 @@
+
+def grade_calculator(assignments:list, bonus_assignment:int, exam: int): # Return tuple[bool, int]
+    grade = (False, 5)
+    # Check that each value is int (ther is most likley a better solution)
+    for i in range(len(assignments)):
+        if assignments[i] is None:
+            assignments[i] = 0
+
+    if bonus_assignment is None:
+        bonus_assignment = 0
+
+    if exam is None:
+        exam = 0
+
+    # Check if >= 50% of all achivable assignment points
+    avrg_point_assignments = sum(assignments) / len(assignments)
+    if avrg_point_assignments < 50:
+        return grade
+    elif exam < 50:
+        return grade
+    else:
+        assignments.sort(reverse=True)
+        for i in range(len(assignments[:7])):
+            if assignments[i] < 25:
+                return grade
+
+    # Calc grade if != 5
+    assignments.append(bonus_assignment)
+    assignments.append(exam)
+    average_final_score = (sum(assignments)/1100)*100
+    if average_final_score >= 87.5:
+        grade = (True, 1)
+        return grade
+    elif 75 <= average_final_score < 87.5:
+        grade = (True, 2)
+        return grade
+    elif 62.5 <= average_final_score < 75:
+        grade = (True, 3)
+    elif not (not (average_final_score >= 50) or not (average_final_score < 62.5)):
+        grade = (True, 4)
+    return grade
+
+
+
+grade_received = grade_calculator([100,100,100,100,100,100,100,100,100,100], 100, 49)
+
+print(grade_received)
