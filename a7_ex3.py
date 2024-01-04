c309967c9f99691e4d630f04c5f46053d35260a1
Moritz <moritz.baldauf24@gmail.com>
2024-01-04 10:28:47 +0100

    A7

--- a7_ex3.py
+++ a7_ex3.py
@@ -3,10 +3,8 @@ class Distance:
         self.x = x
 
     def to_string(self) -> str:
-        # Using type(self).__name__ to get the class name dynamically
         return f"{type(self).__name__}: the number of vectors = {self.x}"
 
     def dist(self) -> float:
-        # This method is not implemented in the base class and is supposed to be overridden in subclasses
         raise NotImplementedError("This method should be implemented by subclasses")
 
